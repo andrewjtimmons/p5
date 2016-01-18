65bf64551bdb658b39b3e518c664c57d6f612a63
andy timmons <andrewjtimmons@gmail.com>
2016-01-18 11:04:02 -0800

    sound test

---
+++ firstSounds/sketch.js
@@ -0,0 +1,20 @@
+function preload() {
+  mySound = loadSound('assets/doorbell.mp3');
+}
+
+function setup() {
+  createCanvas(100, 100);
+  background(0, 255, 0);
+
+  textAlign(CENTER);
+  text('click here to play', width/2, height/2);
+
+  mySound.setVolume(0.1);
+}
+
+// play sound on mouse press over canvas
+function mousePressed() {
+  if (mouseX < width && mouseY < height && mouseX > 0 && mouseY > 0) {
+    mySound.play();
+  }
+}

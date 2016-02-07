cf48c161f88c4e69db4da8baa37148d825881d82
andy timmons <andrewjtimmons@gmail.com>
2016-02-07 15:20:07 -0800

    remove dead test

--- 1x1_with_tests_example/test/test.js
+++ 1x1_with_tests_example/test/test.js
@@ -47,17 +47,6 @@ describe('colorRectangle Unit Tests', function() {
         done();
     });
 
-    // it('should have levels of 255 for red, and 0 for green and blue after calling increaseFillColor 255 times (256 to the power of 3)', function(done) {
-    //     for (var count = 0; count < 16777216; count += 1) {
-    //         testRect.increaseFillColor()
-    //     }
-    //     expect(testRect.currentColor.levels[0]).to.equal(255)
-    //     expect(testRect.currentColor.levels[1]).to.equal(255)
-    //     expect(testRect.currentColor.levels[2]).to.equal(255)
-    //     expect(testRect.incrementCount).to.equal(16777216)
-    //     done();
-    // });
-
     it('should have levels of 255 for red, green, and blue after calling increaseFillColor 16777215 times (256 to the power of 3 - 1)', function(done) {
         //it is 256^3 -1 because it starts with the color black
         for (var count = 0; count < 16777215; count += 1) {
